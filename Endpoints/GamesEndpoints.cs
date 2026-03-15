52d5741bc7ab80980eaf74919371b02c21a96d4c
StevenTmn <102273468+StevenTmn@users.noreply.github.com>
2026-03-15 13:52:22 -0400

    initial Commit

---
+++ Endpoints/GamesEndpoints.cs
@@ -0,0 +1,98 @@
+using System;
+using GameStore.Data;
+using GameStore.Dtos;
+using GameStore.Entities;
+
+namespace GameStore.Endpoints;
+
+
+public static class GamesEndpoints
+{
+    const string GetGameEndpointName = "GetGame";
+
+    private static readonly List<GameDto> games = [
+    new (1, 
+        "The Legend of Zelda: Breath of the Wild", 
+        "Action-Adventure", 
+        59.99m, 
+        new DateOnly(2017, 3, 3)),
+    new (2, 
+        "God of War", 
+        "Action-Adventure", 
+        49.99m, 
+        new DateOnly(2018, 4, 20)),
+    new (3, 
+        "Red Dead Redemption 2", 
+        "Action-Adventure", 
+        59.99m, 
+        new DateOnly(2018, 10, 26))
+];
+
+
+
+
+public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
+{
+    var group = app.MapGroup("games")
+                    .WithParameterValidation();
+
+    // GET /games
+group.MapGet("/", () => games);
+
+// GET /games/{id}
+group.MapGet("/{id}", (int id) => { 
+    GameDto? game = games.Find(game => game.Id == id);
+    
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
+
+// POST /games
+group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) =>
+{
+    Game game = new()
+    {
+        Name = newGame.Name,
+        Genre = dbContext.Genres.Find(newGame.GenreId),
+        GenreId = newGame.GenreId,
+        Price = newGame.Price,
+        ReleaseDate = newGame.ReleaseDate
+    };
+
+    dbContext.Games.Add(game);
+    dbContext.SaveChanges();
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id}, game);
+});
+
+// PUT /games/{id}
+group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) => {
+
+    var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+});
+
+// DELETE /games/{id}
+group.MapDelete("/{id}", (int id) => 
+{
+    games.RemoveAll(game => game.Id == id);
+    return Results.NoContent();
+});
+
+    return group;
+}
+}
